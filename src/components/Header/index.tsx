18c0ba188b4fe481b1862ef266022c0674627aad
dionyso <dionysoleao@gmail.com>
2021-12-23 05:23:59 -0500

    Implement newModal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,17 @@
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
+interface HeaderProps{
+    onOpenNewTransactionModal: () => void
+}
 
-export function Header(){
+export function Header({onOpenNewTransactionModal}:HeaderProps){
+
     return(
         <Container>
             <Content>
             <img src={logoImg} alt="my money" />
-            <button type="button">Nova Transação</button>
+            <button onClick={onOpenNewTransactionModal} type="button">Nova Transação</button>
             </Content>
         </Container>
     )
