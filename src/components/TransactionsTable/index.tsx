631101d222c3eab4a5b213d812b81e29dc916116
dionyso <dionysoleao@gmail.com>
2021-12-25 22:30:55 -0500

    Carregando Transações via Context

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,26 +1,14 @@
-import { useEffect, useState } from "react"
-import { api } from "../../services/api"
+import { useContext } from "react"
+import {  } from "../../services/api"
 import { Container } from "./styles"
-
-interface Transaction {
-    id: number;
-    title: string;
-    amount: number;
-    type: string;
-    category: string;
-    createdAt: string;
-}
+import { TransactionsContext } from '../../TransactionsContext';
 
 
 
 export function TransactionsTable () {
 
-    const[transactions, setTransactions] = useState<Transaction[]>([])
+    const transactions = useContext(TransactionsContext)
 
-    useEffect(() => {
-        api.get('transactions')
-        .then(response => setTransactions(response.data.transactions))
-    } ,[])
 
     return(
         <Container>
